33fcc74dd44fd731a1438fc36d5f74707c843d45
Sabrina Akther <sabrinasobhan.92@gmail.com>
2018-12-17 02:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"]; 
+function kittens() {
+    return kittens;
+}
 // Add your functions and code here
+ 
+function destructivelyAppendKitten(n) {
+    kittens.push(n);
+    return kittens;
+}
+
+function destructivelyPrependKitten(n) {
+    kittens.unshift(n) ;
+    return kittens;
+}
+  
+function destructivelyRemoveLastKitten() {
+    kittens.pop();
+    return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+    kittens.shift();
+    return kittens;
+}
+
+
+
+var appendKitten= function appendKitten(){
+   appendKitten.push("Broom");
+   return appendKitten;
+}
+
+kittens();
+appendKitten();
+  
